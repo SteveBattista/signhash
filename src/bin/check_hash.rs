294c1e03d1cc4e82fcb3f4deea553984afbc9cb0
Stephen Battista <stephen.battista@gmail.com>
2019-08-27 20:26:18 -0400

    Works without signatures only hashes

--- src/bin/check_hash.rs
+++ src/bin/check_hash.rs
@@ -34,10 +34,20 @@ fn main() {
 
     let hashalgo: &Algorithm;
     let inputhash = matches.value_of("algo").unwrap_or("256");
+    let hashlengh_in_bytes :usize;
     match inputhash.as_ref() {
-        "256" => hashalgo = &SHA256,
-        "384" => hashalgo = &SHA384,
-        "512" => hashalgo = &SHA512,
+        "256" => {
+            hashalgo = &SHA256;
+            hashlengh_in_bytes =256;
+        },
+        "384" => {
+            hashalgo = &SHA384;
+            hashlengh_in_bytes =384;
+        },
+        "512" => {
+            hashalgo = &SHA512;
+            hashlengh_in_bytes =512;
+        },
         _ => {
             println!("Please choose 256, 384 or 512 for type of SHA hash.");
             exit(0);
@@ -45,7 +55,7 @@ fn main() {
     }
     //  println!("Hash chosen is {}", inputhash);
 
-    let inputpool = matches.value_of("pool").unwrap_or("10");
+    let inputpool = matches.value_of("pool").unwrap_or("1");
     let poolresult = inputpool.parse();
     let poolnumber;
     match poolresult {
@@ -65,7 +75,7 @@ fn main() {
     pool.scoped(|scoped| {
         for file in inputfiles {
             scoped.execute(move || {
-                let _x = gethashofile(&file, hashalgo);
+                let _x = gethashofile(&file, hashalgo,hashlengh_in_bytes);
             });
         }
     });
@@ -87,25 +97,34 @@ fn var_digest<R: Read>(
     }
     Ok(context.finish())
 }
-fn read_hash(hash_from_file:  & mut [u8], path : &str){
+fn read_hash(hash_from_file:  & mut [u8], path : &str, hashlengh_in_bytes:usize){
     let mut file = File::open(path).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let deserialized_map: BTreeMap<String, String> = serde_yaml::from_str(&contents).unwrap();
     //println!("{}",deserialized_map["Private"]);
-    let  local_key = HEXUPPER.decode(deserialized_map["HASH"].as_bytes()).unwrap();
-    for x in 0..85 {
-        hash_from_file[x] = local_key[x];
+    let local_key_vec = HEXUPPER.decode(deserialized_map["HASH"].as_bytes()).unwrap();
+    //println!("{:?}",local_key_vec);
+    for x in 0..(hashlengh_in_bytes/8) {
+        hash_from_file[x] = local_key_vec[x];
     }
 }
 
-fn gethashofile(path: &str, hashalgo: &'static Algorithm) -> Result<(), Box<dyn Error>> {
+fn gethashofile(path: &str, hashalgo: &'static Algorithm, hashlengh_in_bytes :usize) -> Result<(), Box<dyn Error>> {
     let input = File::open(path)?;
     let reader = BufReader::new(input);
     let digest = var_digest(reader, hashalgo)?;
-    let mut hash_from_file = String::new();
-    read_hash(& mut hash_from_file, & path.to_owned() + ".sig");
-    println!("{} : {}", path, HEXUPPER.encode(digest.as_ref()));
-    println!("{}.sig : {}", path, HEXUPPER.encode(hash_from_file.as_ref()));
+    let mut hash_from_file = [0;85];
+    //println!("{}",&[path, ".sig"].concat());
+    //println!("{}     : {}", path, HEXUPPER.encode(digest.as_ref()));
+    read_hash(& mut hash_from_file, &[path, ".sig"].concat(),hashlengh_in_bytes );
+    //println!("{} : {}", path, HEXUPPER.encode(digest.as_ref()));
+    //println!("{}.sig : {}", path, HEXUPPER.encode(&hash_from_file.as_ref()[..hashlengh_in_bytes/8]));
+    for x in 0..(hashlengh_in_bytes/8) {
+        if hash_from_file[x] != digest.as_ref()[x] {
+            eprintln!("File {} does not match its signature",path);
+            break;
+        }
+    }
     Ok(())
 }
