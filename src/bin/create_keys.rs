293c2b040d21023dfc416950142bc8c28b478e0c
Stephen Battista <stephen.battista@gmail.com>
2019-09-12 08:52:02 -0400

    Works, needs modifaction time, header file and signing entire file

--- src/bin/create_keys.rs
+++ src/bin/create_keys.rs
@@ -1,88 +1,83 @@
 #![forbid(unsafe_code)]
+use std::error::Error;
 use serde_yaml;
 use std::collections::BTreeMap;
-extern crate arrayref;
+//extern crate arrayref;
 use data_encoding::HEXUPPER;
 use ring::signature::KeyPair;
 use std::fs::File;
 use std::io::prelude::*;
+use clap::{App, Arg};
+
+const PRIVATEKEY_LENGH_IN_BYTES: usize = 680;
+const PUBLICKEY_LENGH_IN_BYTES:usize = 256;
 
 fn create_keys(public_key_bytes: &mut [u8], private_key_bytes: &mut [u8]) {
     let rng = ring::rand::SystemRandom::new();
-    let pkcs8_bytes = ring::signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
+    let pkcs8_bytes = match ring::signature::Ed25519KeyPair::generate_pkcs8(&rng){
+        Err(_) => panic!("Couldn't create pks8 key"),
+        Ok(pkcs8_bytes) => pkcs8_bytes,
+        };
 
-    // Normally the application would store the PKCS#8 file persistently. Later
-    // it would read the PKCS#8 file from persistent storage to use it.
 
-    let key_pair = ring::signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+    let key_pair = match ring::signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()){
+        Err(_) => panic!("Couldn't create keypair from pks8 key."),
+        Ok(pkcs8_bytes) => pkcs8_bytes,
+    };
 
-    for x in 0..32 {
+    for x in 0..(PUBLICKEY_LENGH_IN_BYTES/8) {
         public_key_bytes[x] = key_pair.public_key().as_ref()[x];
     }
-    for x in 0..85 {
+    for x in 0..(PRIVATEKEY_LENGH_IN_BYTES/8) {
         private_key_bytes[x] = pkcs8_bytes.as_ref()[x];
     }
 }
-fn write_pubic_key(public_key_bytes: &[u8]) {
-    let mut map = BTreeMap::new();
-    map.insert("Public".to_string(), HEXUPPER.encode(&public_key_bytes));
-    let s = serde_yaml::to_string(&map).unwrap();
-    //println!("{}", s);
-    let mut file = File::create("Signpub.key").unwrap();
-    file.write_all(s.as_bytes()).unwrap();
-}
 
-fn write_private_key(private_key_bytes: &[u8]) {
+fn write_key(public_key_bytes: &[u8], pubic_key_file: &str, key_name: &str) {
     let mut map = BTreeMap::new();
-    map.insert("Private".to_string(), HEXUPPER.encode(&private_key_bytes));
-    let s = serde_yaml::to_string(&map).unwrap();
-    //println!("{}", s);
-    let mut file = File::create("Signpriv.key").unwrap();
-    file.write_all(s.as_bytes()).unwrap();
-}
-
-fn write_keys(public_key_bytes: &[u8], private_key_bytes: &[u8]) {
-    write_pubic_key(&public_key_bytes);
-    write_private_key(&private_key_bytes)
-}
-/*fn read_private_key(private_key_bytes :  & mut [u8]){
-    let mut file = File::open("Signpriv.key").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let deserialized_map: BTreeMap<String, String> = serde_yaml::from_str(&contents).unwrap();
-    //println!("{}",deserialized_map["Private"]);
-    let  local_key = HEXUPPER.decode(deserialized_map["Private"].as_bytes()).unwrap();
-    for x in 0..85 {
-        private_key_bytes[x] = local_key[x];
+    map.insert(key_name.to_string(), HEXUPPER.encode(&public_key_bytes));
+    let s = match serde_yaml::to_string(&map) {
+        Err(_) => panic!("Couldn't create YMAL string for {} key.",key_name),
+        Ok(s) => s,
+    };
+    let mut file = match File::create(&pubic_key_file){
+        Err(why) => panic!("couldn't create {} key at {}: {}", key_name, pubic_key_file, why.description()),
+        Ok(file) => file,
+    };
+    match file.write_all(s.as_bytes()) {
+        Err(why) => panic!("Couldn't write to {} key to {}: {}", key_name, pubic_key_file, why.description()),
+        Ok(_) => println!("Successfully wrote {} key to {}", key_name, pubic_key_file),
     }
 }
 
-fn read_public_key(public_key_bytes :  & mut [u8]){
-    let mut file = File::open("Signpub.key").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let deserialized_map: BTreeMap<String, String> = serde_yaml::from_str(&contents).unwrap();
-    //println!("{}",deserialized_map["Public"]);
-    let  local_key = HEXUPPER.decode(deserialized_map["Public"].as_bytes()).unwrap();
-    for x in 0..32 {
-        public_key_bytes[x] = local_key[x];
-    }
+fn write_keys(public_key_bytes: &[u8], private_key_bytes: &[u8], public_key_file: &str, private_key_file: &str) {
+    write_key(&public_key_bytes, public_key_file,"Public");
+    write_key(&private_key_bytes,private_key_file,"Private");
 }
-*/
 
 fn main() {
-    let mut public_key_bytes: [u8; 32] = [0; 32];
-    let mut private_key_bytes: [u8; 85] = [0; 85];
+    let matches = App::new("Create Keys")
+                          .version("1.0")
+                          .author("Stephen Battista <stephen.battista@gmail.com>")
+                          .about("Creates the private and public keys for a 25519 ecliptic curve keys.")
+                          .arg(Arg::with_name("private")
+                              .short("i")
+                               .long("private")
+                               .value_name("FILE")
+                               .help("This option allows for the user to set the location of the private key. I would reccomend placing it in a location where others can not read it. If not used, Signpriv.key is default. ")
+                               .takes_value(true))
+                         .arg(Arg::with_name("public")
+                                   .short("u")
+                                    .long("public")
+                                    .value_name("FILE")
+                                    .help("This option allows for the user to set the location of the public key. If not used, Signpub.key is default.")
+                                    .takes_value(true))
+                          .get_matches();
+    let private_filename = matches.value_of("private").unwrap_or("Signpriv.key");
+    let pubic_filename = matches.value_of("public").unwrap_or("Signpub.key");
+    let mut public_key_bytes: [u8; (PUBLICKEY_LENGH_IN_BYTES/8)] = [0; (PUBLICKEY_LENGH_IN_BYTES/8)];
+    let mut private_key_bytes: [u8; (PRIVATEKEY_LENGH_IN_BYTES/8)] = [0; (PRIVATEKEY_LENGH_IN_BYTES/8)];
     create_keys(&mut public_key_bytes, &mut private_key_bytes);
-    println!("Public : {}", HEXUPPER.encode(&public_key_bytes));
-    println!("Private : {}", HEXUPPER.encode(&private_key_bytes));
-    write_keys(&public_key_bytes, &private_key_bytes);
-    /*let mut public_key_bytes2: [u8;32]=[0;32];
-    let mut private_key_bytes2 :  [u8;85]= [0;85];
-    read_private_key(& mut private_key_bytes2);
-    read_public_key(& mut public_key_bytes2);
-    println!("Public : {}", HEXUPPER.encode(&public_key_bytes2));
-    println!("Private : {}", HEXUPPER.encode(&private_key_bytes2));
-    assert_eq!(HEXUPPER.encode(&public_key_bytes),HEXUPPER.encode(&public_key_bytes2));
-    assert_eq!(HEXUPPER.encode(&private_key_bytes),HEXUPPER.encode(&private_key_bytes2)); */
+    write_keys(&public_key_bytes, &private_key_bytes,pubic_filename,private_filename);
+
 }
--- src/bin/sign_hash.rs
+++ src/bin/sign_hash.rs
@@ -1,16 +1,26 @@
 #![forbid(unsafe_code)]
+use std::convert::TryInto;
+use rand::prelude::ThreadRng;
+//use std::io::Write;
 use clap::{App, Arg};
 use data_encoding::HEXUPPER;
-use ring::digest::{Algorithm, Context, Digest, SHA256, SHA384, SHA512};
+use ring::digest::{Algorithm, Context, Digest, SHA1_FOR_LEGACY_USE_ONLY, SHA256, SHA384, SHA512};
 use rand::Rng;
 use scoped_threadpool::Pool;
 use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
-use std::io::Write;
+use num_cpus;
 use std::io::{BufReader, Read};
-use std::process::exit;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+
+
+const PRIVATEKEY_LENGH_IN_BYTES: usize = 680;
+const NONCE_LENGH_IN_BYTES: usize = 128;
+const HASH_READ_BUUFER_IN_BYTES: usize = 8192;
 
 fn main() {
     let matches = App::new("sign_hash")
@@ -20,132 +30,179 @@ fn main() {
                           .arg(Arg::with_name("algo")
                                .short("a")
                                .long("algorithm")
-                               .value_name("256 | 384 | 512")
-                               .help("Chooses what algorthim to use SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA256.")
+                               .value_name("128| 256 | 384 | 512")
+                               .help("Chooses what algorthim to use SHA1 -> (128), SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA256. Please don't use SHA1 unless you are using it to line up with threat intelgence.")
                                .takes_value(true))
-                          .arg(Arg::with_name("pool")
-			        .short("p")
-                               .long("pool")
-                               .value_name("#")
-                               .help("Sets the size of the pool of maximum number of concurrent threads when hashing. Default is 10. Large numbers (> 60) may cause the progam not to hash all files.")
-                               .takes_value(true))
-                          .arg(Arg::with_name("files")
-                               .value_name("files")
-                               .help("Place one or more files to hash. Those that can not be found will be ommited from the results. Directories will be ommitted. Links will be treated like normal files.")
+                        .arg(Arg::with_name("private")
+                            .short("i")
+                            .long("private")
+                            .value_name("FILE")
+                            .help("This option allows for the user to set the location of the private key. I would reccomend placing it in a location where others can not read it. If not used, Signpriv.key is default. ")
+                            .takes_value(true))
+                        .arg(Arg::with_name("output")
+                            .short("o")
+                            .long("output")
+                            .value_name("FILE")
+                            .help("This option allows for the user to set the location of the manifest file.  If not used, Manifest.txt is default. ")
+                            .takes_value(true))
+                        .arg(Arg::with_name("pool")
+     			            .short("p")
+                            .long("pool")
+                            .value_name("#")
+                            .help("Sets the size of the pool of maximum number of concurrent threads when hashing. Default is number of CPU cores. Negative numbers set pool to default. Warning: Large numbers (> 60) may cause the progam not to hash all files.")
+                            .takes_value(true))
+                        .arg(Arg::with_name("files")
+                             .value_name("FILES")
+                             .help("Place one or more files to hash. Those that can not be found will be ommited from the results. Directories will be ommitted. Links will be treated like normal files.")
                                .required(true).min_values(1))
-                          .get_matches();
+                        .get_matches();
 
     let hashalgo: &Algorithm;
     let inputhash = matches.value_of("algo").unwrap_or("256");
+    //println!("Hash: {}", inputhash);
     match inputhash.as_ref() {
+        "128" => hashalgo = &SHA1_FOR_LEGACY_USE_ONLY,
         "256" => hashalgo = &SHA256,
         "384" => hashalgo = &SHA384,
         "512" => hashalgo = &SHA512,
         _ => {
-            println!("Please choose 256, 384 or 512 for type of SHA hash.");
-            exit(0);
+            panic!("Please choose 128, 256, 384 or 512 for type of SHA hash.");
         }
     }
-    //  println!("Hash chosen is {}", inputhash);
-
-    let inputpool = matches.value_of("pool").unwrap_or("10");
+    let private_key_file = matches.value_of("private").unwrap_or("Signpriv.key");
+    let _manifest_file_name = matches.value_of("output").unwrap_or("Manifest.txt");
+    let inputpool = matches.value_of("pool").unwrap_or("0");
     let poolresult = inputpool.parse();
-    let poolnumber;
+    /*let _manifest_file = match File::open(& manifest_file_name){
+        Err(why) => panic!("Couldn't open manifest file named {}: {}", manifest_file_name, why.description()),
+        Ok(file) => file,
+    };*/
+    let mut poolnumber;
     match poolresult {
         Ok(n) => poolnumber = n,
         Err(_e) => {
-            println!("Please choose a number for the number of threads.");
-            exit(0);
+            panic!("Please choose a number for the number of threads.");
         }
     }
-    let mut pool = Pool::new(poolnumber);
-    //    println!("pool chosen is {}", inputpool);
+    if poolnumber < 1  {
+        poolnumber = num_cpus::get();
+    }
+    //println!("threads {}",poolnumber);
+    let mut pool = Pool::new(poolnumber.try_into().unwrap());
+    let (tx, rx): (
+        Sender<String>,
+        Receiver<String>,
+    ) = mpsc::channel();
+    let mut children = Vec::new();
 
     let inputfiles: Vec<_> = matches.values_of("files").unwrap().collect();
-    //   println!("Files chosen is {}", inputpool.len());
-    let inputfiles = inputfiles
-        .into_iter()
-        .filter(|&file| !(file.ends_with(".sig")))
-        .collect::<Vec<_>>();
-
+    let num_files = inputfiles.len();
+    let mut private_key_bytes: [u8; (PRIVATEKEY_LENGH_IN_BYTES/8)] = [0; (PRIVATEKEY_LENGH_IN_BYTES/8)];
+    read_private_key(&mut private_key_bytes,private_key_file);
+    let mut nonce_bytes: [u8; (NONCE_LENGH_IN_BYTES / 8)] = [0; (NONCE_LENGH_IN_BYTES / 8)];
+    let rng = rand::thread_rng();
+    let mut nonces: HashMap<[u8; NONCE_LENGH_IN_BYTES / 8], i32> = HashMap::new();
     pool.scoped(|scoped| {
         for file in inputfiles {
-            scoped.execute(move || {
-                let _x = gethashofile(&file, hashalgo);
+            let thread_tx = tx.clone();
+            provide_unique_nonce(& mut nonce_bytes,& mut nonces,rng);
+            let child = scoped.execute(move || {
+                let _x = create_line(&file, hashalgo, &nonce_bytes, &private_key_bytes,thread_tx);
             });
+            children.push(child);
         }
     });
+
+    let mut strings = Vec::new();
+    for _ in 0..num_files {
+        // The `recv` method picks a message from the channel
+        // `recv` will block the current thread if there are no messages available
+        strings.push(rx.recv().unwrap());
+    }
+
+    // Show the order in which the messages were sent
+    for _ in 0..strings.len() {
+        println!("{}",strings.remove(0));
+}
 }
 
-fn read_private_key(private_key_bytes: &mut [u8]) {
-    let mut file = File::open("Signpriv.key").unwrap();
+fn provide_unique_nonce(nonce_bytes: &mut [u8; 16], nonces: &mut std::collections::HashMap<[u8; 16], i32>, mut rng : ThreadRng ){
+    let mut duplicate = true;
+    let mut number :u8;
+    while duplicate {
+        duplicate = false;
+        for x in 0..(NONCE_LENGH_IN_BYTES / 8) {
+            number = rng.gen();
+            nonce_bytes[x] = number;
+        }
+        if nonces.contains_key(nonce_bytes) {
+            duplicate = true;
+            eprintln!("//Duplicated nonce {} making a new one", HEXUPPER.encode(nonce_bytes));
+        } else {
+            nonces.insert(*nonce_bytes, 0);
+        }
+    }
+
+}
+
+fn read_private_key(private_key_bytes: &mut [u8], private_key_file: &str) {
+    let mut file = match File::open(& private_key_file){
+        Err(why) => panic!("Couldn't open private key file named {}: {}", private_key_file, why.description()),
+        Ok(file) => file,
+    };
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let deserialized_map: BTreeMap<String, String> = serde_yaml::from_str(&contents).unwrap();
-    //println!("{}",deserialized_map["Private"]);
-    let local_key = HEXUPPER
-        .decode(deserialized_map["Private"].as_bytes())
-        .unwrap();
-    for x in 0..85 {
+    let local_key = match HEXUPPER.decode(deserialized_map["Private"].as_bytes()) {
+        Err(why) => panic!("Couldn't decode hexencoded private key: {}", why.description()),
+        Ok(local_key) => local_key,
+    };
+    for x in 0..(PRIVATEKEY_LENGH_IN_BYTES/8) {
         private_key_bytes[x] = local_key[x];
     }
-    //println!("{}", HEXUPPER.encode(private_key_bytes));
 }
 
 fn var_digest<R: Read>(
     mut reader: R,
     hashalgo: &'static Algorithm,
-) -> Result<Digest, Box<dyn Error>> {
+) -> Digest {
     let mut context = Context::new(hashalgo);
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; (HASH_READ_BUUFER_IN_BYTES/8)];
 
     loop {
-        let count = reader.read(&mut buffer)?;
+        let count = reader.read(&mut buffer).unwrap();
         if count == 0 {
             break;
         }
         context.update(&buffer[..count]);
     }
-    Ok(context.finish())
-}
-fn write_sigfile(hash: &[u8], path: &str, filen: u64, nonce: &[u8], sig: &[u8]) {
-    let mut map = BTreeMap::new();
-    map.insert("LENGTH".to_string(), filen.to_string());
-    map.insert("HASH".to_string(), HEXUPPER.encode(&hash));
-    map.insert("NONCE".to_string(),HEXUPPER.encode(&nonce));
-    map.insert("SIG".to_string(), HEXUPPER.encode(&sig));
-    let s = serde_yaml::to_string(&map).unwrap();
-    //println!("{}", s);
-    let new_path = path.to_owned() + ".sig";
-    //println!("{}",new_path);
-    let mut file = File::create(new_path).unwrap();
-    file.write_all(s.as_bytes()).unwrap();
+    context.finish()
 }
 
-fn sign_sigfile(hash: &[u8], filen: u64, nonce: &[u8], private_key_bytes: &[u8]) -> ring::signature::Signature {
-    let data = format!("{}:{}:{}", HEXUPPER.encode(&hash), filen.to_string(),HEXUPPER.encode(&nonce)) ;
-    //println!("{}",data);
+fn sign_sigfile(path: &str, filen: u64, hash: &[u8], nonce: &[u8], private_key_bytes: &[u8]) -> ring::signature::Signature {
+    let data = format!("{}:{}:{}:{}:", path, filen.to_string(),HEXUPPER.encode(&hash),HEXUPPER.encode(&nonce)) ;
     let key_pair = ring::signature::Ed25519KeyPair::from_pkcs8(private_key_bytes.as_ref()).unwrap();
     let sig = key_pair.sign(data.as_bytes());
     return sig;
 }
 
-fn gethashofile(path: &str, hashalgo: &'static Algorithm) -> Result<(), Box<dyn Error>> {
-    let input = File::open(path)?;
-    let reader = BufReader::new(input);
-    let digest = var_digest(reader, hashalgo)?;
+fn create_line(path: &str, hashalgo: &'static Algorithm, nonce_bytes: &[u8], private_key_bytes : &[u8], tx : std::sync::mpsc::Sender<String>){
     let metadata = fs::metadata(path).unwrap();
-    let filelen = metadata.len();
-    let mut private_key_bytes: [u8; 85] = [0; 85];
-    let mut rng = rand::thread_rng();
-    let mut number: u8;
-    let mut nonce_bytes : [u8; 16] = [0; 16];
-    for x in 0..16 {
-        number = rng.gen();
-        nonce_bytes[x] = number;
+
+    if !(metadata.is_dir()) {
+        let filelen = metadata.len();
+        let input = File::open(path).unwrap();
+        let reader = BufReader::new(input);
+        let digest = var_digest(reader, hashalgo);
+
+        let signature = sign_sigfile(path, filelen, &digest.as_ref(), &nonce_bytes, &private_key_bytes);
+        let data = format!("{}:{}:{}:{}:{}", path, filelen.to_string(),HEXUPPER.encode(&digest.as_ref()),HEXUPPER.encode(&nonce_bytes),HEXUPPER.encode(&signature.as_ref())) ;
+        //println!("{}",data);
+        tx.send(data).unwrap();
     }
-    read_private_key(&mut private_key_bytes);
-    let sig = sign_sigfile(&digest.as_ref(), filelen, &nonce_bytes, &private_key_bytes);
-    write_sigfile(&digest.as_ref(), path, filelen, &nonce_bytes, sig.as_ref());
-    Ok(())
+    else {
+        let data = format!("Directory: {}", path);
+     //println!("{}",data);
+     tx.send(data).unwrap();
+    }
 }
