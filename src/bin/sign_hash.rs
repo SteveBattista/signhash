9fc6f12c419ff5b4116ba6a33fb2371f83707792
Stephen Battista <stephen.battista@gmail.com>
2019-09-24 06:05:39 -0400

    Fixed to include manifest version 0.5.0

--- src/bin/sign_hash.rs
+++ src/bin/sign_hash.rs
@@ -1,27 +1,25 @@
 #![forbid(unsafe_code)]
 
-
-use signhash::write_key;
 use signhash::create_keys;
-use signhash::write_headers;
-use signhash::provide_unique_nonce;
-use signhash::write_from_channel;
-use signhash::Message;
 use signhash::create_line;
+use signhash::provide_unique_nonce;
+use signhash::write_headers;
+use signhash::write_key;
+use signhash::write_manifest_from_channel;
+use signhash::SignMessage;
+use signhash::HEADER_MESSAGE_COUNT;
+use signhash::NONCE_LENGTH_IN_BYTES;
 use signhash::PRIVATEKEY_LENGTH_IN_BYTES;
 use signhash::PUBLICKEY_LENGTH_IN_BYTES;
-use signhash::NONCE_LENGTH_IN_BYTES;
 
-
-
-use std::convert::TryInto;
 use scoped_threadpool::Pool;
+use std::convert::TryInto;
 
 use num_cpus;
 use std::thread;
 
-use clap::{App, Arg};
 use chrono::{DateTime, Utc};
+use clap::{App, Arg};
 
 use ring::digest::{Algorithm, SHA1_FOR_LEGACY_USE_ONLY, SHA256, SHA384, SHA512};
 
@@ -32,8 +30,8 @@ use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::time::Instant;
 
-use std::io::Write;
 use std::io::stdout;
+use std::io::Write;
 
 use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
@@ -48,12 +46,18 @@ fn main() {
                           .version("0.1.0")
                           .author("Stephen Battista <stephen.battista@gmail.com>")
                           .about("Implements a signed hash for files")
-                          .arg(Arg::with_name("algo")
+                          .arg(Arg::with_name("hash")
                                .short("a")
-                               .long("algorithm")
+                               .long("hash")
                                .value_name("128| 256 | 384 | 512")
-                               .help("Chooses what algorthim to use SHA1 -> (128), SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA256. Please don't use SHA1 unless you are using it to line up with threat intelgence.")
+                               .help("Chooses what hash algorthim to use SHA1 -> (128), SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA256. Please don't use SHA1 unless you are using it to line up with threat intelgence.")
                                .takes_value(true))
+                               .arg(Arg::with_name("signing")
+                                    .short("s")
+                                    .long("signing")
+                                    .value_name("ED25519")
+                                    .help("Chooses what signing algorthim to use ED25519. Default is ED25519. Will implement post quantum signature at some point.")
+                                    .takes_value(true))
                           .arg(Arg::with_name("public")
                                 .short("u")
                                 .long("public")
@@ -64,7 +68,7 @@ fn main() {
                             .short("o")
                             .long("output")
                             .value_name("FILE")
-                            .help("This option allows for the user to set the location of the manifest file.  If not used, Manifest.txt is default. ")
+                            .help("This option allows for the user to set the location of the manifest file.  If not used, STDIO is default. ")
                             .takes_value(true))
                         .arg(Arg::with_name("pool")
      			            .short("p")
@@ -72,9 +76,9 @@ fn main() {
                             .value_name("#")
                             .help("Sets the size of the pool of maximum number of concurrent threads when hashing. Default is number of CPU cores. Negative numbers set pool to default. Warning: Large numbers (> 60) may cause the progam not to hash all files.")
                             .takes_value(true))
-                            .arg(Arg::with_name("header")
-         			            .short("h")
-                                .long("header")
+                            .arg(Arg::with_name("inlcude")
+         			            .short("i")
+                                .long("include")
                                 .value_name("FILE")
                                 .help("Name of file that you would like to include in the header.")
                                 .takes_value(true))
@@ -82,12 +86,12 @@ fn main() {
                             .short("d")
                             .long("directory")
                              .value_name("DIRECTORY")
-                             .help("Place one directory. Default is current working directory.Those that can not be found will be ommited from the results. Directories will be ommitted. Links will be treated like normal files.")
+                             .help("Directory to start hashing. Default is current working directory.Those that can not be found will be ommited from the results. Directories will be ommitted. Links will be treated like normal files.")
                              .takes_value(true))
                         .get_matches();
 
     let hashalgo: &Algorithm;
-    let inputhash = matches.value_of("algo").unwrap_or("256");
+    let inputhash = matches.value_of("hash").unwrap_or("256");
     match inputhash.as_ref() {
         "128" => hashalgo = &SHA1_FOR_LEGACY_USE_ONLY,
         "256" => hashalgo = &SHA256,
@@ -95,6 +99,13 @@ fn main() {
         "512" => hashalgo = &SHA512,
         _ => {
             panic!("Please choose 128, 256, 384 or 512 for type of SHA hash.");
+        }
+    }
+    let signing = matches.value_of("signing").unwrap_or("ED25519");
+    match signing.as_ref() {
+        "ED25519" => (),
+        _ => {
+            panic!("Please choose ED25519 for type of signature algorthrim.");
         }
     }
     let manifest_file = matches.value_of("output").unwrap_or("|||").to_string();
@@ -120,36 +131,36 @@ fn main() {
 
     let mut pool = Pool::new(poolnumber.try_into().unwrap());
 
-    let (tx, rx): (Sender<Message>, Receiver<Message>) = mpsc::channel();
-
+    let (tx, rx): (Sender<SignMessage>, Receiver<SignMessage>) = mpsc::channel();
 
     let header_file = matches.value_of("header").unwrap_or("|||");
 
     let input_directoy = matches.value_of("directory").unwrap_or(".");
 
     let mut inputfiles: Vec<String> = Vec::new();
-    let bar = ProgressBar::new_spinner();
+    let spinner = ProgressBar::new_spinner();
     if fileoutput {
-        bar.set_prefix("Constucting file list time:");
-        bar.set_style(ProgressStyle::default_bar()
-    .template("{prefix}{elapsed_precise} {spinner}"));
+        spinner.set_prefix("Constucting file list time:");
+        spinner.set_style(
+            ProgressStyle::default_bar().template("{prefix} {elapsed_precise} {spinner}"),
+        );
     }
     for entry in WalkDir::new(input_directoy) {
         //println!("{}", entry.unwrap().path().display());
         inputfiles.push(entry.unwrap().path().display().to_string());
         if fileoutput {
-            bar.tick();
+            spinner.tick();
         }
     }
     if fileoutput {
-        bar.finish();
+        spinner.finish();
     }
 
     let num_files = inputfiles.len();
     let mut private_key_bytes: [u8; (PRIVATEKEY_LENGTH_IN_BYTES / 8)] =
         [0; (PRIVATEKEY_LENGTH_IN_BYTES / 8)];
     let mut public_key_bytes: [u8; (PUBLICKEY_LENGTH_IN_BYTES / 8)] =
-            [0; (PUBLICKEY_LENGTH_IN_BYTES / 8)];
+        [0; (PUBLICKEY_LENGTH_IN_BYTES / 8)];
 
     create_keys(&mut public_key_bytes, &mut private_key_bytes);
     write_key(&public_key_bytes, public_key_file, "Public");
@@ -158,31 +169,38 @@ fn main() {
     let rng = rand::thread_rng();
     let mut nonces: HashMap<[u8; NONCE_LENGTH_IN_BYTES / 8], i32> = HashMap::new();
 
-
-    write_headers(&tx,inputhash,&args.join(" "),header_file,&now,poolnumber);
+    write_headers(
+        &tx,
+        inputhash,
+        &args.join(" "),
+        header_file,
+        &now,
+        poolnumber,
+    );
     let bar = ProgressBar::new(inputfiles.len().try_into().unwrap());
     if fileoutput {
         bar.set_prefix("Number of Files Hashed");
-        bar.set_style(ProgressStyle::default_bar()
-        .template("{prefix} {wide_bar} {pos}/{len} {elapsed_precise}"));
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template("{prefix} {wide_bar} {pos}/{len} {elapsed_precise}"),
+        );
     }
     let writer_child = thread::spawn(move || {
-        write_from_channel(
-            num_files + signhash::HEADER_MESSAGES,
+        write_manifest_from_channel(
+            num_files + HEADER_MESSAGE_COUNT,
             hashalgo,
             &private_key_bytes,
             rx,
             start,
             manifest_file,
             &bar,
-            fileoutput
+            fileoutput,
         );
     });
 
-     pool.scoped(|scoped| {
+    pool.scoped(|scoped| {
         stdout().flush().unwrap();
         for file in inputfiles {
-
             let thread_tx = tx.clone();
             provide_unique_nonce(&mut nonce_bytes, &mut nonces, rng);
             scoped.execute(move || {
@@ -192,13 +210,9 @@ fn main() {
                     &nonce_bytes,
                     &private_key_bytes,
                     thread_tx,
-
                 );
             });
-
-
         }
-
     });
     let _res = writer_child.join();
 }
